feddace5a19d5024cc3a2af374c0dee71680f006
Armen Matevosyan <armen@matevosyan.am>
2024-04-07 19:24:35 +0400

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.4"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.polarbookshop"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webflux")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
